380ed0b85c14dccd034b3353ca87ea5bf2aa02d6
TheBabblingBrin <bhooverlee27@gmail.com>
2022-09-26 21:45:39 -0400

    GET details of playlistbyId

--- backend/routes/api/playlists.js
+++ backend/routes/api/playlists.js
@@ -3,7 +3,7 @@ const { setTokenCookie, restoreUser, requireAuth} = require('../../utils/auth');
 
 const router = express.Router();
 
-const { Song, User, Album, Playlist } = require('../../db/models');
+const { Song, User, Album, Playlist, PlaylistSong } = require('../../db/models');
 
 //CREATE playlist
 router.post('/', requireAuth, async(req, res, next)=>{
@@ -13,6 +13,21 @@ router.post('/', requireAuth, async(req, res, next)=>{
   res.json(newPL)
 })
 
+//ADD song to playlist by Id
+router.post('/:playlistId/songs', requireAuth, async (req, res, next)=>{
+  const playlistId = parseInt(req.params.playlistId)
+  const {songId} = req.body
+  const addSong = await PlaylistSong.create({playlistId: playlistId, songId: songId})
+  res.json(addSong)
+})
 
-
+//GET details of playlist by Id
+router.get('/:playlistId', async (req, res, next)=>{
+  const playlist = await Playlist.findByPk(req.params.playlistId, {
+    include:{
+      model: Song,
+    }
+  })
+  res.json(playlist)
+})
 module.exports = router;
