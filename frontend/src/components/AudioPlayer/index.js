879c1b2e25d5e90552c1147e9ab3b0f6611bab81
TheBabblingBrin <bhooverlee27@gmail.com>
2022-10-16 21:44:28 -0400

    heroku production test of music player

--- frontend/src/components/AudioPlayer/index.js
+++ frontend/src/components/AudioPlayer/index.js
@@ -1,17 +1,22 @@
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
-// import 'react-h5-audio-player/lib/styles.less' Use LESS
-// import 'react-h5-audio-player/src/styles.scss' Use SASS
+import { useSelector, useDispatch } from 'react-redux';
 
-const Player = () => (
+
+
+const Player = () => {
+  const song = useSelector(state => state.songs)
+
+  return (
   <footer>
     <AudioPlayer
       autoPlay
-      src="https://cdn.whyp.it/cf3349cc-62ee-4ac2-a2a9-ed91659e18ca.mp3"
+      src={song[1].url}
       onPlay={e => console.log("onPlay")}
       // other props here
     />
   </footer>
-);
+  )
+  };
 
 export default Player
