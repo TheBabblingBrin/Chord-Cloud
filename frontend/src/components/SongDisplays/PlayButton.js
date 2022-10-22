4c9f0e8d535d031266eec166029117a25d8f1efe
TheBabblingBrin <bhooverlee27@gmail.com>
2022-10-21 23:23:51 -0400

    fixed spacing between songs/titles on song cards

--- frontend/src/components/SongDisplays/PlayButton.js
+++ frontend/src/components/SongDisplays/PlayButton.js
@@ -1,24 +1,19 @@
 import React, {useEffect, useState} from 'react';
-import { Link, useHistory} from 'react-router-dom';
 import { useDispatch, useSelector} from 'react-redux';
 import { setCurrentSong } from '../../store/songs';
 
 
 const PlayButton = ({ song }) => {
   const dispatch = useDispatch()
-  const history = useHistory()
   const currentSong = useSelector(state => state.songs.currentSong)
   const [button, setButton] = useState(<i className="fa-solid fa-play"></i>)
   const playing = useSelector(state => state.songs.playing)
 
   useEffect(()=>{
-    const pressed = document.getElementById(`play-id-${song.id}`)
     if(playing === true && currentSong.id === song.id){
       setButton(<i className="fa-solid fa-pause"></i>)
-      // pressed.style.opacity = '1'
     }else{
       setButton(<i className="fa-solid fa-play"></i>)
-      // pressed.style.opacity = '0'
     }
 
 
@@ -29,7 +24,6 @@ const PlayButton = ({ song }) => {
   return (
       <button
       className='play-button'
-      id={`play-id-${song.id}`}
       onClick={()=>
         dispatch(setCurrentSong(song))}>
         {button}
