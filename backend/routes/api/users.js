1224743afafbf8b4d229d0f0f0f44d79e161330b
TheBabblingBrin <bhooverlee27@gmail.com>
2022-10-24 08:38:10 -0400

    spelling on an error validation

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -35,7 +35,7 @@ const validateSignup = [
   check('password')
     .exists({ checkFalsy: true })
     .isLength({ min: 6, max: 30})
-    .withMessage('Password must be 6 and 30 characters.'),
+    .withMessage('Password must be between 6 and 30 characters.'),
   handleValidationErrors
 ];
 
