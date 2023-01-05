f88e5e40945db4e2042711fde94beda6a3ef8ccc
TheBabblingBrin <bhooverlee27@gmail.com>
2023-01-05 16:12:28 -0500

    removed demo song button

--- frontend/src/components/SongFormModal/SongForm.js
+++ frontend/src/components/SongFormModal/SongForm.js
@@ -39,21 +39,9 @@ const SongForm = ({song, formType = 'createSong', setShowModal}) => {
   },[title, url, imageUrl, setSubmitted])
 
 
-  let demoSong = null
-  if(user?.id ===1){
-    demoSong = (<button type="button"
-    onClick={()=>{
-      setTitle('test song 1');
-      setDescription('dev test');
-      // setImage('https://cdn.pixabay.com/audio/2022/08/05/13-29-08-266_200x200.png');
-      setUrl('https://cdn.pixabay.com/audio/2022/08/04/audio_2dde668d05.mp3');
-      // setAlbumId(1)
-    }
-    }>Demo Song</button>)
-  }
+
   const updateFile = (e) => {
     const file = e.target.files[0];
-    console.log('TARGET FILES', e.target.files)
     if (file) setImage(file);
   };
 
@@ -122,33 +110,29 @@ const SongForm = ({song, formType = 'createSong', setShowModal}) => {
           value={description}
           onChange={updateDescription}
         />
-        {/* <input
-          type="text"
-          placeholder="Audio URL"
-          value={url}
-          onChange={updateUrl}
-        /> */}
-        <input type="file" value={''} onChange={updateSong} />
 
-         {/* <input
-          type="text"
-          placeholder="Image URL"
-          value={imageUrl}
-          onChange={updateImage}
-        /> */}
-        <input type="file" value={''} onChange={updateFile} />
+            <label for='file-upload' className='spot-upload-label'>
+                        Song :
+                        <input
+                            id='file-upload'
+                            type="file"
+                            // value={''}
+                            onChange={updateSong} />
+                    </label>
 
-         {/* <input
-          type="number"
-          placeholder="Album ID"
-          value={albumId}
-          onChange={updateAlbumId}
-        /> */}
+        <label for='file-upload' className='spot-upload-label'>
+                        Image :
+                        <input
+                            id='file-upload'
+                            type="file"
+                            // value={''}
+                            onChange={updateFile} />
+                    </label>
+
         <button
           type="submit"
           // disabled={errors.length > 0}
         >{formType === 'createSong'? 'Upload Song':'Update Song'}</button>
-        {demoSong}
         {hasSubmitted && errors?.length > 0 && (
 
                     <ErrorList errors={errors}/>
